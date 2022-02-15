079491a6aa6d4371b82e1a8419be87485bd63c44
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-02-15 11:50:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StringDataStore<int> grades = new StringDataStore<int>();
+            grades[0] = 100;
+            grades[1] = 300;
+            grades[2] = 150;
+            grades[3] = 590;
+            grades[4] = 900;
+            grades[5] = 600;
+
+            for(int i = 0; i < grades.Length; i++)
+                Console.WriteLine(grades[i]);
+        }
+    }
+}
